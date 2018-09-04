8398907b6e9cbe79ffb59bccc0613e0e162911bc
Sofian Brabez <sofian.brabez@gonitro.com>
2018-09-04 10:52:14 +0100

    Switch to Table Driven Tests strategy. cf [1]
    [1] https://github.com/golang/go/wiki/TableDrivenTests

--- nginx-nr-agent_test.go
+++ nginx-nr-agent_test.go
@@ -6,7 +6,7 @@ import (
 	httpmock "gopkg.in/jarcoal/httpmock.v1"
 )
 
-func NginxStatusReponseString() {
+func NginxStatusReponseString() string {
 	return `Active connections: 2
 server accepts handled requests
  31 30 42
@@ -19,25 +19,25 @@ func Test_GetStats(t *testing.T) {
 
 	httpmock.RegisterResponder("GET", "http://localhost/status",
 		httpmock.NewStringResponder(200, NginxStatusReponseString()))
-
 	metric, _ := GetStats("http://localhost/status")
-	if metric.Connections != 2 {
-		t.Errorf("Want %d, Got %d", 2, metric.Connections)
+
+	var metricTests = []struct {
+		want int64
+		got  int64
+	}{
+		{metric.Connections, 2},
+		{metric.Accepts, 31},
+		{metric.Handled, 30},
+		{metric.Requests, 42},
+		{metric.Reading, 0},
+		{metric.Writing, 10},
+		{metric.Waiting, 1},
 	}
-	if metric.Accepts != 31 {
-		t.Errorf("Want %d, Got %d", 31, metric.Accepts)
-	}
-	if metric.Handled != 30 {
-		t.Errorf("Want %d, Got %d", 30, metric.Handled)
-	}
-	if metric.Reading != 0 {
-		t.Errorf("Want %d, Got %d", 0, metric.Reading)
-	}
-	if metric.Writing != 10 {
-		t.Errorf("Want %d, Got %d", 10, metric.Writing)
-	}
-	if metric.Waiting != 1 {
-		t.Errorf("Want %d, Got %d", 1, metric.Waiting)
+
+	for _, input := range metricTests {
+		if input.want != input.got {
+			t.Errorf("Want %d, Got %d", input.want, input.got)
+		}
 	}
 
 }
